67a6e48e4101fb3255d8922a3aee20895194ad84
Nikita Posled <poslednikita@gmail.com>
2018-04-07 16:27:24 +0300

    Add choise for turn on/off cascade delete. Add  commets "UI" for features to add to UILayer  (check Kernel, DataBaseInstance and all InternalDataBaseInstaceComponents)

--- Database/DataLayer/App/DataBaseInstance/InternalDataBaseInstanceComponents/LinkColumn.cs
+++ Database/DataLayer/App/DataBaseInstance/InternalDataBaseInstanceComponents/LinkColumn.cs
@@ -13,7 +13,7 @@ namespace DataModels.App.InternalDataBaseInstanceComponents
     public class LinkColumn : Column
     {
         Column linkedColumn;
-        public LinkColumn(string name, Type DataType, bool allowsnull, object def, Column linkedcolumn) : base(name, DataType, allowsnull, def)
+        public LinkColumn(string name, Type DataType, bool allowsnull, object def, Table thisTable, Column linkedcolumn) : base(name, DataType, allowsnull, def, thisTable)
         {
             if (linkedcolumn.IsPkey)
             {
@@ -30,14 +30,14 @@ namespace DataModels.App.InternalDataBaseInstanceComponents
            
         }
 
-        public override void EditColumnElementByPrimaryKey(Table thisTable, int key, object argument)
+        public override void EditColumnElementByPrimaryKey(int key, object argument)
         {
-            if (thisTable.isTableContainsData())
+            if (ThisTable.isTableContainsData())
             {
                 if (DataType == argument.GetType())
                 {
                     if (isLinkedColumnContainsSuchValue(argument))
-                        DataList[thisTable.returnIndexOfPrimaryKey(key)].Data = argument;
+                        DataList[ThisTable.returnIndexOfPrimaryKey(key)].Data = argument;
                     else throw new ArgumentException("There is no such argument ("+argument.ToString()+") in linkedColumn (" + linkedColumn.Name + ")!");
                 }
                 else throw new ArgumentException("Type of argument is not similar to Column type");
